981016f55542bd73b7b89a014175eafa082aa950
LilianeFalcao <85350811+LilianeFalcao@users.noreply.github.com>
2022-06-29 19:48:31 -0300

    aaaaaaaaaaa

--- src/screens/Sair/index.tsx
+++ src/screens/Sair/index.tsx
@@ -1,32 +1,31 @@
 import { AxiosError } from "axios";
 import React, { useEffect, useState } from "react";
 import { Alert, View } from "react-native";
-import { useAuth } from "../../hook/auth";
-import { IUser } from "../../interfaces/User.interface";
 import Loading from "../../components/Loading";
+import { useAuth } from "../../Hook/auth";
+import { IUser } from "../../interfaces/User.interface";
 
-export default function Sair (){
-    const {signOut} = useAuth();
-    const [isLoading , setIsLoading] = useState(true);
-
+export default function Sair() {
+    const { signOut } = useAuth();
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-        async function logout(){
+        async function logout() {
             try {
                 await signOut();
-            } catch (error) {
+            } 
+            catch (error) {
                 const err = error as AxiosError;
                 const data = err.response?.data as IUser;
                 let message = "";
                 if (data.data) {
-                    for(const [key, value] of Object.entries(data.data)){
+                    for (const [key, value] of Object.entries(data.data)) {
                         message = `${message} ${value}`;
                     }
                 }
-                Alert.alert(`${data.message} ${message}`);
+            Alert.alert(`${data.message} ${message}`);
             }
         }
         logout();
-    },[]);
-
+    }, []);
     return <>{isLoading ? <Loading /> : <View />}</>;
 }
